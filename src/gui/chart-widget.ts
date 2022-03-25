51386a5f47059d00a2f1866b57f445546ec94393
teka <leiwulong@gshopper.com>
2022-03-25 19:11:17 +0900

    testing height

--- src/gui/chart-widget.ts
+++ src/gui/chart-widget.ts
@@ -375,7 +375,7 @@ export class ChartWidget implements IDestroyable {
 			paneHeight = Math.max(calculatePaneHeight, 2);
 
 			// eslint-disable-next-line no-console
-			console.log('_adjustSizeImpl: paneHeight', paneHeight, this._paneWidgets);
+			console.log('_adjustSizeImpl: paneHeight', paneHeight, paneWidget.state(), paneWidget);
 
 			accumulatedHeight += paneHeight;
 
